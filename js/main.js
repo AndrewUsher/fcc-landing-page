61b09bed3c4b2d0e999237acfd7dbd6f2260a7ca
Andrew Usher <andrewusher00@gmail.com>
2018-01-24 19:11:30 -0600

    Fix misspelled variable

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var header = document.querySelector('header')
+const header = document.querySelector('header')
 
 window.addEventListener('scroll', function () {
   let scrollPosition = window.scrollY;
-  if (scrollpos > 200) {
+  if (scrollPosition > 200) {
     header.classList.add('header-bg');
   } else {
     header.classList.remove('header-bg');
